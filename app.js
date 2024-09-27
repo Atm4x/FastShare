f8286bed69f6c7e4f821e1005b82ac29695211e4
artem hakaev <mrtaktplay@gmail.com>
2024-09-27 20:52:39 +0300

    port test

--- app.js
+++ app.js
@@ -19,7 +19,6 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static('public'));
-app.use(express.json());
 
 // Секретный ключ для JWT
 const JWT_SECRET = crypto.randomBytes(64).toString('hex');
